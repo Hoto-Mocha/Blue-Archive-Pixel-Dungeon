0efaa5bbe27be53c12fab05b4221339bf6f641cf
Cocoa <mtj2010@naver.com>
2023-07-22 03:59:04 +0900

    타이틀 배너 수정

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/effects/BannerSprites.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/effects/BannerSprites.java
@@ -38,19 +38,19 @@ public class BannerSprites {
 		Image icon = new Image( Assets.Interfaces.BANNERS );
 		switch (type) {
 			case PIXEL_DUNGEON:
-				icon.frame( icon.texture.uvRect( 0, 0, 132, 90 ) );
+				icon.frame( icon.texture.uvRect( 0, 0, 188, 103 ) );
 				break;
 			case BOSS_SLAIN:
-				icon.frame( icon.texture.uvRect( 0, 90, 128, 125 ) );
+				icon.frame( icon.texture.uvRect( 0, 210, 128, 245 ) );
 				break;
 			case GAME_OVER:
-				icon.frame( icon.texture.uvRect( 0, 125, 128, 160 ) );
+				icon.frame( icon.texture.uvRect( 0, 245, 128, 280 ) );
 				break;
 			case SELECT_YOUR_HERO:
-				icon.frame( icon.texture.uvRect( 0, 160, 128, 181 ) );
+				icon.frame( icon.texture.uvRect( 0, 280, 128, 301 ) );
 				break;
 			case PIXEL_DUNGEON_SIGNS:
-				icon.frame( icon.texture.uvRect( 132, 0, 256, 90 ) );
+				icon.frame( icon.texture.uvRect( 0, 106, 188, 210 ) );
 				break;
 		}
 		return icon;

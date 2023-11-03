b23a885986cee7ca254ed0dbaec53bca24f23c42
Cocoa <mtj2010@naver.com>
2023-11-03 10:27:04 +0900

    랭킹 화면 아이템 슬롯 색깔 수정

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndRanking.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndRanking.java
@@ -481,7 +481,7 @@ public class WndRanking extends WndTabbed {
 		@Override
 		protected void createChildren() {
 			
-			bg = new ColorBlock( 28, HEIGHT, 0x9953564D );
+			bg = new ColorBlock( 28, HEIGHT, 0x60DFDFDF );
 			add( bg );
 			
 			slot = new ItemSlot();
@@ -548,7 +548,7 @@ public class WndRanking extends WndTabbed {
 
 		@Override
 		protected void createChildren() {
-			bg = new ColorBlock( 1, 1, 0x9953564D );
+			bg = new ColorBlock( 1, 1, 0x60DFDFDF );
 			add( bg );
 
 			super.createChildren();
